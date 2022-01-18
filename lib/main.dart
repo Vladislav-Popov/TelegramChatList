770e35c8a4cea69c042f37875e612cfdd5ef9894
Vladislav <57004611+Vladislav-Popov@users.noreply.github.com>
2022-01-18 20:31:06 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Чаты'),
+    );
+  }
+}
+
+final List<MenuRowData> secondMenuRow = [
+  MenuRowData(
+      Icons.notifications_on, 'Архив', 'Фрольченков, Бар на диване, коктейли'),
+  MenuRowData(Icons.privacy_tip, 'Оля',
+      'что dsdffgh jklhkgjhfj klhkgjfhdgjhkj jldfkjlksjdflj jkljsdlkjldksjто'),
+  MenuRowData(Icons.storage, 'Папа', 'Иван, сын моего однокласс=ника'),
+  MenuRowData(Icons.brush, 'Данила', 'Видео'),
+  MenuRowData(Icons.language, 'Килограмм срача',
+      'Салават Газизов ну поднималовь до 132'),
+  MenuRowData(Icons.print, 'LazyLoad Dart', 'Dmitry Dmitry'),
+  MenuRowData(Icons.airplane_ticket, 'Новороссийск ДТП', 'ДТП было в 5 утра.'),
+];
+
+class MenuRowData {
+  final IconData avatar;
+  final String contactName;
+  final String textMessage;
+
+  MenuRowData(this.avatar, this.contactName, this.textMessage);
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // int _counter = 0;
+  //
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            labelStyle: TextStyle(
+              letterSpacing: 0.1,
+              fontSize: 12,
+            ),
+            tabs: [
+              Text('Все'),
+              Text('New'),
+              Text('Personal'),
+              Text('Unread'),
+              Text('Job'),
+            ],
+          ),
+          leading: Center(
+            child: Text(
+              'Изм.',
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          title: Text(widget.title),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              child: Icon(Icons.note),
+            )
+          ],
+        ),
+        body: ListView(
+          children: secondMenuRow.map((e) => WidgetRow(data: e)).toList(),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.contact_page),
+              label: 'Контакты',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.call),
+              label: 'Звонки',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Чаты',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Настройки',
+            ),
+          ],
+          selectedItemColor: Colors.blue,
+        ),
+      ),
+    );
+  }
+}
+
+class WidgetRow extends StatelessWidget {
+  final MenuRowData data;
+  const WidgetRow({Key? key, required this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 25,
+            child: Icon(
+              data.avatar,
+              size: 40,
+            ),
+          ),
+          SizedBox(
+            width: 16,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  data.contactName,
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  data.textMessage,
+                  maxLines: 1,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
